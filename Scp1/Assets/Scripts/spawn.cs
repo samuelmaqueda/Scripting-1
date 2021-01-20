d7db82d8569995bea80c362511ec9d7e30a3e6c4
Samuel Maqueda <samuel.maqueda@hotmail.com>
2021-01-20 23:26:18 +0100

    Dia 1

---
+++ Scp1/Assets/Scripts/spawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn : MonoBehaviour
+{
+	public bool dentro;
+	public GameObject enemigo;
+	public float contador;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (dentro == true)
+		{
+			contador += Time.deltaTime;
+			if (contador >= 3f)
+			{
+				contador = 0f;
+				Instantiate(enemigo, transform.position, transform.rotation);
+			}
+		}
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "player")
+		{
+			dentro = true;
+		}
+		
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		if (other.tag == "player")
+		{
+			dentro = false;
+		}
+	}
+}
